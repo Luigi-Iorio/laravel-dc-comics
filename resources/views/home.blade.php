fd3c54998baba9acd2473f0b231414938b8a0adb
Luigi-Iorio <luigi0iorio@gmail.com>
2024-02-01 22:13:35 +0100

    feat: link for redirect to form page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,8 @@
         <div class="home">
             <h2>Clicca il bottone per visualizzare la lista completa dei fumetti</h2>
             <a href="{{ route('comics.index') }}">Fumetti</a>
+            <h2>Clicca il bottone per aggiungere un nuovo fumetto</h2>
+            <a href="{{ route('comics.create') }}">Aggiungi</a>
         </div>
     </main>
 @endsection
