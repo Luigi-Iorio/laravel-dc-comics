5cb2afb689dc53c6f7a7a9efafe8b31a843f5813
Luigi-Iorio <luigi0iorio@gmail.com>
2024-02-01 16:14:31 +0100

    feat: insert data in comics db - seeders

---
+++ database/seeders/ComicsTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Comic;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $comics = config('comics_db'); //file contenete array di array associativi
+
+        foreach ($comics as $comic) { //ad ogni iterazione vengono eseguiti i passaggi successivi
+            $newComic = new Comic(); //istanza del Model
+            $newComic->title = $comic['title'];
+            $newComic->description = $comic['description'];
+            $newComic->thumb = $comic['thumb'];
+            $newComic->price = $comic['price'];
+            $newComic->series = $comic['series'];
+            $newComic->sale_date = $comic['sale_date'];
+            $newComic->type = $comic['type'];
+
+            $newComic->save(); //salvare nel db
+        }
+    }
+}
