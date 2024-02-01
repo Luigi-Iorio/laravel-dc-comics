0b5994747e9a5003db0d90c9e4fc55a110dcfabe
Luigi-Iorio <luigi0iorio@gmail.com>
2024-02-01 22:01:33 +0100

    style: customization show page

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,22 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout.main')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Comics</title>
-</head>
-
-<body>
-    <h1>Fumetto Selezionato</h1>
-    <ul>
-        <li>{{ $comic['title'] }}</li>
-        <li>{{ $comic['price'] }}</li>
-        <li>{{ $comic['series'] }}</li>
-        <li>{{ $comic['sale_date'] }}</li>
-        <li>{{ $comic['type'] }}</li>
-    </ul>
-</body>
-
-</html>
+@section('main')
+    <main>
+        <div class="show">
+            <h2>Fumetto Selezionato</h2>
+            <ul>
+                <li>Titolo: {{ $comic['title'] }}</li>
+                <li>Prezzo: $ {{ $comic['price'] }}</li>
+                <li>Serie: {{ $comic['series'] }}</li>
+                <li>Data: {{ $comic['sale_date'] }}</li>
+                <li>Tipo: {{ $comic['type'] }}</li>
+            </ul>
+        </div>
+    </main>
+@endsection
